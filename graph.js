9b7b83ca967e75ef08b544312da254962a87b8e1
Rani Abu Sultan <raniabusultan@gmail.com>
2019-02-16 13:37:19 +0300

    Nodes Enter Selection

--- graph.js
+++ graph.js
@@ -27,8 +27,28 @@ const update = (data) => {
   // console.log(treeData);
 
   // get node selection and join data
-  const nodes = graph.selectAll('.node')
+  const nodes = graph.selectAll('.node') // same as of using 'g'
     .data(treeData.descendants())
+
+  // create enter node groups
+  const enterNodes = nodes.enter()
+    .append('g')
+      .attr('class', 'node')
+      .attr('transform', d => `translate(${d.x}, ${d.y})`);
+
+  // append rects to enter nodes
+  enterNodes.append('rect')
+    .attr('fill', '#aaa')
+    .attr('stroke', '#555')
+    .attr('stroke-width', 2)
+    .attr('height', 50)
+    .attr('width', d => d.data.name.length * 20) // calculate width based on length of the name
+
+  // append name text
+  enterNodes.append('text')
+    .attr('text-anchor', 'middle')
+    .attr('fill', 'black')
+    .text(d => d.data.name)
   
 }
 
