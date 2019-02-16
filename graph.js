2ce79dcc6c301f45e67856cf09cbc3083c1a7756
Rani Abu Sultan <raniabusultan@gmail.com>
2019-02-16 12:50:31 +0300

    Graph Setup

--- graph.js
+++ graph.js
@@ -1,5 +1,14 @@
-// data & firebase hook-up
+const dims = { height: 500, width: 1100 };
 
+const svg = d3.select('.canvas')
+  .append('svg')
+  .attr('width', dims.width + 100)
+  .attr('height', dims.height + 100);
+
+const graph = svg.append('g')
+  .attr('transform', 'translate(50, 50)')
+
+// data & firebase hook-up
 var data = [];
 
 db.collection('employees').onSnapshot(res => {
